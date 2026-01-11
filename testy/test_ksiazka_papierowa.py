dc4a7f7d61601f76a0e76397c6ca7f65e6cbf579
Wawrzyniec Michalczyk <wawrzyniec.michalczyk@ue.wroc.pl>
2026-01-11 12:57:17 +0100

    komentarze zad1

--- testy/test_ksiazka_papierowa.py
+++ testy/test_ksiazka_papierowa.py
@@ -1,25 +1,4 @@
-# from encje.IKsiazka import IKsiazka
-#
-# class KsiazkaPapierowa(IKsiazka):
-#     def __init__(self, tytul: str, autor: str, ISBN: int, gatunek: str,
-#                  cena: float, stanMagazynowy: int, opis: str):
-#         super().__init__(tytul, autor, ISBN, gatunek, cena, opis)
-#         self.stanMagazynowy: int = stanMagazynowy
-#
-#     def ustawTytul(self, tytul: str) -> None:
-#         self.tytul = tytul
-#
-#     def ustawAutora(self, autor: str) -> None:
-#         self.autor = autor
-#
-#     def ustawGatunek(self, gatunek: str) -> None:
-#         self.gatunek = gatunek
-#
-#     def ustawCene(self, cena: float) -> None:
-#         self.cena = cena
-#
-#     def ustawOpis(self, opis: str) -> None:
-#         self.opis = opis
+
 
 def tag(*tags):
     def decorator(func):
@@ -33,7 +12,7 @@ from encje.KsiazkaPapierowa import KsiazkaPapierowa
 class TestKsiazkaPapierowa(unittest.TestCase):
 
     def setUp(self):
-        # GIVEN – przygotowanie danych wejściowych testu
+        # GIVEN – przygotowanie danych
         self.ksiazka = KsiazkaPapierowa(
             tytul="Test",
             autor="Autor",
